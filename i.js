f2e229e115c88f2ad4945eaee835a5153f74892f
GrayOrchid <jorajostart@bk.ru>
2023-05-10 17:29:27 +0300

    2.0

--- i.js
+++ i.js
@@ -1,9 +1,9 @@
 // MENU
 let bodyPage = document.body
 let menu = document.querySelector('.menu')
-let menuOpenBtn = document.querySelector('.menu--open-btn')
-let menuClose = document.querySelector('.menu--close')
-let menuNavigationLink = document.querySelectorAll('.menu--navigation-link')
+let menuOpenBtn = document.querySelector('.menu__open-btn')
+let menuClose = document.querySelector('.menu__close')
+let menuNavigationLink = document.querySelectorAll('.menu__navigation-link')
 
 menuNavigationLink.forEach((e) =>
    e.addEventListener('click', menuCloseFunction)
@@ -27,25 +27,28 @@ window.addEventListener('scroll', goToTop)
 function goToTop(params) {
    let scrollPositon = scrollY
    let top = document.querySelector('.top')
+
    if (scrollPositon > 100) {
-      top.classList.add('top--active')
+      top.classList.add('top__active')
    } else {
-      top.classList.remove('top--active')
+      top.classList.remove('top__active')
    }
 }
-let switchTheme = document.querySelectorAll('.theme-switch-btn ').forEach((e)=>{e.addEventListener('click',darkMode)})
+
+// THEME SWITCH
+let switchTheme = document.querySelectorAll('.theme-switch-btn ').forEach((e) => { e.addEventListener('click', darkMode) })
 
 function darkMode(params) {
 
-   let contactLinks = document.querySelectorAll('.footer--contact-link')
+   let contactLinks = document.querySelectorAll('.footer__contact-link')
    let whiteSmokeColor = document.querySelectorAll('.--f6f6f6')
    let activeLink = document.querySelectorAll('.--active-link')
-   let menuThemeSwitch = document.querySelector('.menu--theme-switch-btn')
-   
+   let menuThemeSwitch = document.querySelector('.menu__theme-switch-btn')
+
    menuThemeSwitch.classList.toggle('--white-color')
-   whiteSmokeColor.forEach((e)=>e.classList.toggle('--nero'))
-   contactLinks.forEach((e)=>e.classList.toggle('--white-color'))
-   activeLink.forEach((e)=>e.classList.toggle('--white-color'))
+   whiteSmokeColor.forEach((e) => e.classList.toggle('--nero'))
+   contactLinks.forEach((e) => e.classList.toggle('--white-color'))
+   activeLink.forEach((e) => e.classList.toggle('--white-color'))
    menu.classList.toggle('dark-mode-menu')
    bodyPage.classList.toggle('dark-mode')
 }
